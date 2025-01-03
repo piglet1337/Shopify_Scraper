dc34ee491af99a34da85223cec9b78764b2eced7
piglet1337 <72782432+piglet1337@users.noreply.github.com>
2025-01-03 15:27:35 -0800

    made it faster

--- findAllShops.py
+++ findAllShops.py
@@ -2,6 +2,8 @@ import urllib.request
 import re
 
 def get_html(shopify_url):
+    if not shopify_url.startswith("https://"):
+        shopify_url = "https://" + shopify_url
     try:
         fp = urllib.request.urlopen(shopify_url)
         mybytes = fp.read()
@@ -15,6 +17,18 @@ def get_html(shopify_url):
 def find_links(html):
     links = re.findall('https:\/\/[\w_-]+(?:(?:\.[\w_-]+)+)[\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]', html)
     return links
+
+def thin_links(links, shopify_url):
+    url = shopify_url.replace("https://", "")
+    url = url.replace("www.", "")
+    domains = url.split(".")[:-1]
+    thin_links = [shopify_url]
+    for link in links:
+        for domain in domains:
+            if domain in link:
+                thin_links.append(link)
+                break
+    return thin_links
 
 def get_shop(url):
     html_as_string = get_html(url)
@@ -34,8 +48,9 @@ def get_all_shops(links):
     return shops
 
 if __name__ == "__main__":
-    shopify_url = "https://us.gotyu-underwear.com/"
+    shopify_url = "https://4t2.run/"
     html = get_html(shopify_url)
     links = find_links(html)
+    links = thin_links(links, shopify_url)
     shops = get_all_shops(links)
     print(shops)
