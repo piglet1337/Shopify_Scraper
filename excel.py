911fc641f0d276e1eddc0582c16600bc011938fc
piglet1337 <72782432+piglet1337@users.noreply.github.com>
2025-02-18 01:53:13 -0800

    working with excel

---
+++ excel.py
@@ -0,0 +1,37 @@
+import pandas
+import findAllShops
+
+def read_excel(file):
+    data_frame = pandas.read_excel(file, header = 1)
+    return data_frame
+
+def extract_domain_column(df):
+    return df['domain']
+
+def findShops_InsertIntoDF(x, bucket7, notBucket7):
+    shops = list(findAllShops.find_all_shopify_shops(x))
+    if len(shops) <= 1:
+        notBucket7.append({0:x,1:shops[0]})
+        return
+    row = {0:x}
+    i = 1
+    for shop in shops:
+        row[i] = shop
+        i += 1
+    bucket7.append(row)
+    
+
+if __name__ == "__main__":
+    bucket7 = []
+    notBucket7 = []
+
+    df = read_excel('domains_export.xlsx')
+    df = extract_domain_column(df)
+    df.apply(findShops_InsertIntoDF, args=[bucket7, notBucket7])
+    bucket7DF = pandas.DataFrame(bucket7)
+    notBucket7DF = pandas.DataFrame(notBucket7)
+    print(bucket7DF)
+    print(notBucket7DF)
+    with pandas.ExcelWriter('myShopify.xlsx', engine='xlsxwriter') as writer:
+        bucket7DF.to_excel(writer, sheet_name='bucket7')
+        notBucket7DF.to_excel(writer, sheet_name='notBucket7')
